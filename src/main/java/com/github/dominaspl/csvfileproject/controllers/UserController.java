5151e1a88d22a720099da0e173ea4af23be33aa8
DominasPL <dominik-stepuch@wp.pl>
2019-10-26 17:25:34 +0200

    Create User entity, dto and controller
    Created user entity with necessarry attributes. DTO to convert data
    from String and also controller to display page with file loading

--- src/main/java/com/github/dominaspl/csvfileproject/controllers/UserController.java
+++ src/main/java/com/github/dominaspl/csvfileproject/controllers/UserController.java
@@ -4,6 +4,11 @@ import com.github.dominaspl.csvfileproject.dtos.RegisterDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class UserController {
@@ -15,5 +20,30 @@ public class UserController {
         return "register";
     }
 
+    @PostMapping
+    private String addUsersFromGivenFile(@RequestParam("file") MultipartFile file) {
 
+        String data = convertFileToString(file);
+
+        if(data.isEmpty()) {
+            throw new IllegalStateException("Incorrect data given!");
+        }
+
+
+
+        return "redirect:/";
+    }
+
+    public String convertFileToString(MultipartFile file) {
+
+        String data = "";
+
+        try {
+            data = new String(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
 }
--- src/main/webapp/WEB-INF/views/register.jsp
+++ src/main/webapp/WEB-INF/views/register.jsp
@@ -8,7 +8,10 @@
 
     <h1>Add new file</h1>
 
-    
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file"> <br>
+        <input type="submit" value="submit">
+    </form>
 
 </body>
 </html>
